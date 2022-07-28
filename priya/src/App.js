a1a925a04818c31fff5b022996be53eb93fd6211
maheshMyDocSpace <mahesh@mydocspace.com>
2022-07-28 07:52:08 +0530

    initial commit

---
+++ priya/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import EventItem from './components/EventItem';
+function App() {
+  // JSX - Java Script XML
+  
+  return (
+    <div className="App">
+      <h2> This is React App...</h2>
+      <EventItem></EventItem>
+    </div>
+  );
+}
+
+export default App;
