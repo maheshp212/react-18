9e8d9ef64917d9499f44dc8af2212e570e7aa6a8
Mahesh Perla <maheshp212@gmail.com>
2023-01-07 18:53:40 +0530

    expenses

--- rs/src/components/main/main.js
+++ rs/src/components/main/main.js
@@ -4,6 +4,7 @@ import Form from "../forms/forms";
 import Home from "../home/home";
 import Redux from "../redux/redux";
 import { Route, Routes } from "react-router-dom";
+import Expenses from "../expenses/expenses";
 /** Switch became Routes from react router dom V6 */
 
 const MainComponent = () => {
@@ -29,6 +30,7 @@ const MainComponent = () => {
         <Route path="/forms" element={<Form/>}/>
         <Route path="/apis" element={<Apis/>}/>
         <Route path="/redux" element={<Redux/>}/>
+        <Route path="/expenses" element={<Expenses/>}/>
       </Routes>
       
     </main>
--- rs/src/components/menu/menu.js
+++ rs/src/components/menu/menu.js
@@ -4,6 +4,7 @@ const Menu = () =>{
     return <nav>
         <ul>
             <li><Link to="/home">Home</Link></li>
+            <li><Link to="/expenses">Expenses</Link></li>
             <li><Link to="/forms">Forms</Link></li>
             <li><Link to="/api">API</Link></li>
             <li><Link to="/redux">Redux</Link></li>
