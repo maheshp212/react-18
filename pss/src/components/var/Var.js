232ba46d65279d372528938af7e1b4db6f9a0106
Mahesh Perla <maheshp212@gmail.com>
2024-01-27 09:23:33 +0530

    var

--- pss/src/components/var/Var.js
+++ pss/src/components/var/Var.js
@@ -1,8 +1,51 @@
 import "./Var.css";
 
 const Var  = function(){
-    
-    return <div> Var component</div>
+    let fname = "pss";
+    let age = 34;
+    let user = {
+        name : 'react',
+        exp: 3,
+    };
+    let interest = null;
+    let gender = true;
+    let fruits = ['kiwi', 'melon', 'berry'];
+    let undf;
+
+    let a = 2;
+    let b = 3;
+    return <div > Var component<br/> <br/><br/> <br/>
+        {fname} <br/> <br/>
+        
+        {age} <br/> <br/>
+        
+        **{interest} <br/> <br/>
+        
+        **{gender} <br/> <br/>
+        
+        **{undf} <br/> <br/>
+        {fruits[0]} <br/> <br/>
+        
+        {user.name} <br/> <br/>
+        
+        a+b :: {a+b} <br/> <br/>
+        a-b :: {a-b} <br/> <br/>
+        a*b :: {a*b} <br/> <br/>
+        a/b :: {a/b} <br/> <br/>
+        a%b :: {a%b} <br/> <br/> <br/> <br/>
+
+
+        2+3 :: {2+3} <br/><br/>
+        2-3 :: {2-3} <br/><br/>
+        2*3 :: {2*3} <br/><br/>
+        2/3 :: {2/3} <br/><br/>
+        2%3 :: {2%3} <br/><br/>
+
+
+        {/* string Interpolation
+        data binding
+        expressions */}
+    </div>
 }
 
 export default Var;
