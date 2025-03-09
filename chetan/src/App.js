5730c0b094583961ebfd4f7ee3fda77c9b7e06b7
Mahesh Perla <maheshp212@gmail.com>
2025-03-09 19:13:53 +0530

    chetan added

---
+++ chetan/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Chetan Training</h1>
+      </header>
+      <nav>
+        <div> <a href='#'>Home</a></div>
+        <div> <a href='#'>Intro</a></div>
+        <div> <a href='#'>Var</a></div>
+      </nav>
+      <main>
+        gthis is contnet
+      </main>
+      <footer>  &copy; 2025 ; </footer>
+    </>
+  );
+}
+
+export default App;
